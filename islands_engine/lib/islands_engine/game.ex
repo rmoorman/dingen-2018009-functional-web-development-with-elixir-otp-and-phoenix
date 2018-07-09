aabee6453639b86dfcab9d6e3c94f481de20af29
Rico Moorman <rico.moorman@gmail.com>
2018-07-09 22:10:50 +0200

    Guess a Coordinate

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -29,6 +29,9 @@ defmodule IslandsEngine.Game do
   def set_islands(game, player) when player in @players, do:
     GenServer.call(game, {:set_islands, player})
 
+  def guess_coordinate(game, player, row, col) when player in @players, do:
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+
 
 
   ###
@@ -103,6 +106,29 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state) do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state, opponent_key)
+    rules = state.rules
+    with {:ok, rules} <-
+           Rules.check(rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <-
+           Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <- Rules.check(rules, {:win_check, win_status})
+    do
+      state
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error -> {:reply, :error, state}
+      {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+    end
+  end
+
 
 
   ###
@@ -127,5 +153,16 @@ defmodule IslandsEngine.Game do
 
   defp update_board(state, player, board), do:
     Map.update!(state, player, fn player -> %{player | board: board} end)
+
+
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
+
+
+  defp update_guesses(state, player_key, hit_or_miss, coordinate) do
+    update_in(state[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
 
 end
